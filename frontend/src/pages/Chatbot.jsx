4c91a2cc83cf5149a01c34315e59f7f455fd122e
CodeWithDubeyji <dubeyj313@gmail.com>
2025-01-07 15:08:50 +0530

    Basic UI structure of the WebApp

---
+++ frontend/src/pages/Chatbot.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../components/Header'
+const Chatbot = () => {
+  return (
+    <div className='flex-1 z-10 overflow-auto relative'>
+      <Header title="Chatbot"/>
+    </div>
+  )
+}
+
+export default Chatbot
